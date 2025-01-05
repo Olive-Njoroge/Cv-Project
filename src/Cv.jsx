0f3d53f984707af7e6c754849fb8c90a799014c4
Olive Njoroge <njorogeolive@gmail.com>
2025-01-05 21:03:06 +0300

    Added a section for Education.

--- src/Cv.jsx
+++ src/Cv.jsx
@@ -3,6 +3,11 @@ function Cv(){
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [number, setNumber] = useState("");
+    const [school, setSchool] = useState("");
+    const [study, setStudy] = useState("");
+    const [location, setLocation] = useState("");
+    const [qualification, setQualification] = useState("");
+    const [graduation, setGraduation] = useState("");
 
     const addName = (e) => {
         setName(e.target.value)
@@ -16,15 +21,47 @@ function Cv(){
         setNumber(e.target.value)
     }
 
+    const addSchoolName = (e) => {
+        setSchool(e.target.value)
+    }
+
+    const addFieldOfStudy = (e) => {
+        setStudy(e.target.value)
+    }
+
+    const addSchoolLocation = (e) => {
+        setLocation(e.target.value)
+    }
+
+    const addQualification = (e) => {
+        setQualification(e.target.value)
+    }
+
+    const addGraduationYear = (e) => {
+        setGraduation(e.target.value)
+    }
+
     return(
         <>
-        <div>
+        <div className="general-information">
             <input type='text' value={name} onChange={addName}/>
             <p>Name: {name}</p>
             <input type="email" value={email} onChange={addEmail}/>
             <p>Email: {email}</p>
             <input value={number} onChange={addPhoneNumber}/>
             <p>Phone Number: {number}</p>
+        </div>
+        <div className="educational-experience">
+            <input type="text" value={school} onChange={addSchoolName}/>
+            <p>School Name: {school}</p>
+            <input type="text" value={study} onChange={addFieldOfStudy}/>
+            <p>Field of study: {study}</p>
+            <input type="text" value={location} onChange={addSchoolLocation}/>
+            <p>School Location: {location}</p>
+            <input type="text" value={qualification} onChange={addQualification} placeholder='e.g. Diploma, Degree, A levels' />
+            <p>Qualification: {qualification}</p>
+            <input type="text" value={graduation} onChange={addGraduationYear} placeholder='2010'/>
+            <p>Graduation Year: {graduation}</p>
         </div>
         </>
     )
