ca8b7623a6fe4f19e7b17ba69fc872ab70cc8130
Olive Njoroge <njorogeolive@gmail.com>
2025-01-06 13:54:51 +0300

    More Changes

--- src/Cv.jsx
+++ src/Cv.jsx
@@ -1,79 +1,84 @@
 import React, {useState} from 'react'
 function Cv(){
-    const [fname, setFname] = useState("");
-    const [lname, setLname] = useState("");
-    const [email, setEmail] = useState("");
-    const [number, setNumber] = useState("");
-    const [school, setSchool] = useState("");
-    const [study, setStudy] = useState("");
-    const [location, setLocation] = useState("");
-    const [qualification, setQualification] = useState("");
-    const [graduation, setGraduation] = useState("");
-    const [job, setJob] = useState("");
-    const [employer, setEmployer] = useState("");
-    const [startDate, setStartDate] = useState("");
-    const [endDate, setEndDate] = useState("");
-    const [city, setCity] = useState("");
-    const [country, setCountry] = useState("");
+    const [name, setName] = useState({fName:"", lName: "", email: "", number: ""});
+    const [school, setSchool] = useState({name: "", study: "", loaction: "", qualification: "", graduation: ""});
+    const [job, setJob] = useState({job: "", employer: "", startDate: "", endDate: "", city: "", country: ""});
+    const [skills, setSkills] = useState(["writing", "dxx"]);
+    const [isSkillInputVisisble, setIsSkillInputVisible] = useState(false);//toggles visibility
 
     const addFname = (e) => {
-        setFname(e.target.value)
+        setName(n => ({...n, fName: e.target.value}))
     }
 
      const addLname = (e) => {
-        setLname(e.target.value)
+        setName (n => ({...n, lName: e.target.value}))
     }
 
     const addEmail = (e) => {
-        setEmail(e.target.value)
+        setName(n => ({...n, email: e.target.value}))
     }
 
     const addPhoneNumber = (e) => {
-        setNumber(e.target.value)
+        setName(n => ({...n, number: e.target.value}))
     }
 
     const addSchoolName = (e) => {
-        setSchool(e.target.value)
+        setSchool(s => ({...s, name: e.target.value}))
     }
 
     const addFieldOfStudy = (e) => {
-        setStudy(e.target.value)
+        setSchool(s => ({...s, study:e.target.value}))
     }
 
     const addSchoolLocation = (e) => {
-        setLocation(e.target.value)
+        setSchool(s => ({...s, location: e.target.value}))
     }
 
     const addQualification = (e) => {
-        setQualification(e.target.value)
+        setSchool(s => ({...s, qualification: e.target.value}))
     }
 
     const addGraduationYear = (e) => {
-        setGraduation(e.target.value)
+        setSchool(s => ({...s, graduation: e.target.value}))
     }
 
     const addJob = (e) => {
-        setJob(e.target.value)
+        setJob(j => ({...j, job: e.target.value}))
     }
 
     const addEmployer = (e) => {
-        setEmployer(e.target.value)
+        setJob(j => ({...j, employer: e.target.value}))
     }
 
     const addStartDate = (e) => {
-        setStartDate(e.target.value)
+        setJob(j => ({...j, startDate: e.target.value}))
     }
 
     const addEndDate = (e) => {
-        setEndDate(e.target.value)
+        setJob(j => ({...j, endDate: e.target.value}))
     }
 
     const addCity = (e) => {
-        setCity(e.target.value)
+        setJob(j => ({...j, city: e.target.value}))
     }
 
     const addCountry = (e) => {
-        setCountry(e.target.value)
+        setJob(j => ({...j, country: e.target.value}))
+    }
+
+    const addSkill = () => {
+        const newSkill = document.getElementById("newSkill").value;
+        document.getElementById("newSkill").value = "";
+        setSkills(s => [...s, newSkill]);
+        setIsSkillInputVisible(false);//Hide input after adding skill
+    }
+
+    const removeSkill = (index) => {
+        setSkills(skills.filter((_,i) => i !== index));
+    }
+
+    const toggleSkillInput = () => {
+        setIsSkillInputVisible(!isSkillInputVisible);//toggle input visibility
     }
 
     return(
@@ -81,57 +86,74 @@ function Cv(){
         <div className="input">
             <div className="general-information">
             <h1>General Information</h1>
-            <p>First Name: {fname}</p>
-            <input type='text' value={fname} onChange={addFname}/>
-            <p>Last Name: {lname}</p>
-            <input type='text' value={lname} onChange={addLname}/>
-            <p>Email: {email}</p>
-            <input type="email" value={email} onChange={addEmail}/>
-            <p>Phone Number: {number}</p>
-            <input value={number} onChange={addPhoneNumber}/>
+            <p>First Name: {name.fName}</p>
+            <input type='text' value={name.fName} onChange={addFname}/>
+            <p>Last Name: {name.lName}</p>
+            <input type='text' value={name.lName} onChange={addLname}/>
+            <p>Email: {name.email}</p>
+            <input type="email" value={name.email} onChange={addEmail}/>
+            <p>Phone Number: {name.number}</p>
+            <input value={name.number} onChange={addPhoneNumber}/>
         </div>
 
         <div className="educational-experience">
             <h1>Education</h1>
-            <p>School Name: {school}</p>
-            <input type="text" value={school} onChange={addSchoolName}/>
-            <p>Field of study: {study}</p>
-            <input type="text" value={study} onChange={addFieldOfStudy}/>
-            <p>School Location: {location}</p>
-            <input type="text" value={location} onChange={addSchoolLocation}/>
-            <p>Qualification: {qualification}</p>
-            <input type="text" value={qualification} onChange={addQualification} placeholder='e.g. Diploma, Degree, A levels' />
-            <p>Graduation Year: {graduation}</p>
-            <input type="text" value={graduation} onChange={addGraduationYear} placeholder='2010'/>
+            <p>School Name: {school.name}</p>
+            <input type="text" value={school.name} onChange={addSchoolName}/>
+            <p>Field of study: {school.study}</p>
+            <input type="text" value={school.study} onChange={addFieldOfStudy}/>
+            <p>School Location: {school.location}</p>
+            <input type="text" value={school.location} onChange={addSchoolLocation}/>
+            <p>Qualification: {school.qualification}</p>
+            <input type="text" value={school.qualification} onChange={addQualification} placeholder='e.g. Diploma, Degree, A levels' />
+            <p>Graduation Year: {school.graduation}</p>
+            <input type="text" value={school.graduation} onChange={addGraduationYear} placeholder='2010'/>
         </div>
 
         <div className="practical-experience">
             <h1>Experience</h1>
-            <p>Job Title: {job}</p>
-            <input type="text" value={job} onChange={addJob}/>
-            <p>Employer: {employer}</p>
-            <input type="text"  value={employer} onChange={addEmployer}/>
-            <p>Start Date: {startDate}</p>
-            <input type="text" value={startDate} onChange={addStartDate}/>
-            <p>End Date: {endDate}</p>
-            <input type="text" value={endDate} onChange={addEndDate} />
-            <p>City: {city}</p>
-            <input type="text" value={city} onChange={addCity} />
-            <p>Country: {country}</p>
-            <input type="text" value={country} onChange={addCountry}/>
+            <p>Job Title: {job.job}</p>
+            <input type="text" value={job.job} onChange={addJob}/>
+            <p>Employer: {job.employer}</p>
+            <input type="text"  value={job.employer} onChange={addEmployer}/>
+            <p>Start Date: {job.startDate}</p>
+            <input type="text" value={job.startDate} onChange={addStartDate}/>
+            <p>End Date: {job.endDate}</p>
+            <input type="text" value={job.endDate} onChange={addEndDate} />
+            <p>City: {job.city}</p>
+            <input type="text" value={job.city} onChange={addCity} />
+            <p>Country: {job.country}</p>
+            <input type="text" value={job.country} onChange={addCountry}/>
         </div>
 
         <div className="skills">
             <h1>⭐Skills</h1>
-            <button>+ Add Skill</button>
-        </div>
-        </div>
+            
+            
+                <ul>
+                {skills.map((skill, index) => 
+                <li key={index} onClick={() => removeSkill(index)}>{skill}</li>)}
+            </ul>
 
-        <div className="cv">
+            {isSkillInputVisisble && (
+                <div className='add-skill-input'><input type="text" id='newSkill' />
+            <button onClick={addSkill}>+</button>
+            </div>
+            )}
+            
+            
+
+            <button onClick={toggleSkillInput}>+ Add Skill</button>
+        </div>
         </div>
         
-        </>
-    )
 
+       
+        
+
+
+</>
+)
 }
+
 export default Cv
